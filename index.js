725e1432bfaad289f02065a42698c175232a5a96
birmas <birmingh@gmail.com>
2018-07-16 17:44:30 +1000

    updated example to include GetBlockHeight()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Neon = require('@cityofzion/neon-js');
 const neon = Neon.default;
 const config = {
-  "contract-scripthash": "902a9622de1083bcceeeb9e5e2223f468ecaf0cf",
+  "contract-scripthash": "eac77fffef895a3bc90ca97437f30f9794e007da",
   "neo-rpc-seed": "http://seed2.neo.org:20332",
   "addresses": ['AYus7RaBLTBAzf4xid5WefSxQmTeeQHsR4', 'AcpoeLaicSDJSyv9ZEtLnGXKzqthnExsud']
 };
@@ -39,8 +39,13 @@ function AccountPortalData(walletAddress, groupNumber) {
     operation: 'GetGroupUnlockBlock',
     args: [groupNumber]
   };
+  const getCurrentBlockHeight = {
+    scriptHash: config['contract-scripthash'],
+    operation: 'GetBlockHeight',
+    args: []
+  };
 
-  const script = neon.create.script(getBalance, getGroupMaxContribution, getGroupUnlockBlock);
+  const script = neon.create.script(getBalance, getGroupMaxContribution, getGroupUnlockBlock, getCurrentBlockHeight);
   Neon.rpc.Query.invokeScript(script)
     .execute(config['neo-rpc-seed'])
     .then(res => {
@@ -48,7 +53,8 @@ function AccountPortalData(walletAddress, groupNumber) {
       const contractValues = {
         userBalance: Neon.u.Fixed8.fromReverseHex(stack[0].value === '' ? '00' : stack[0].value),
         groupMax: parseInt(Neon.u.reverseHex(stack[1].value), 16),
-        groupUnlockBlock: parseInt(Neon.u.reverseHex(stack[2].value), 16) || 0
+        groupUnlockBlock: parseInt(Neon.u.reverseHex(stack[2].value), 16) || 0,
+        blockHeight: stack[3].value
       };
       // console.log(stack);
       console.log(contractValues);
